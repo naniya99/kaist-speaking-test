e9909fb122fbd30f0751b6be657130ab258a707d
naniya99 <naniya99@github.com>
2026-07-12 17:09:34 +0900

    Add picture test 3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3880995616" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3834990596" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
